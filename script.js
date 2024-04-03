7f2498b68ee9ce6e4b295b2e1714a2a2d7c5d5ca
vtg <varastothgergo@ktch.hu>
2024-04-03 11:30:47 +0200

    kesz

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 console.log("Helló")
 url = "https://jsonplaceholder.typicode.com/posts"
 console.log(url)
+data=[]
+oszlopok = ["name","age","address","job","hobby"]
+
+function render(){
+    container = document.getElementsByClassName("container")[0]
+    container.innerHTML=""
+
+    sor = document.createElement('div')
+    sor.className="row"
+
+    for(const oszlop of oszlopok){
+        cella = document.createElement('div')
+        cella.className= "col"
+        cella.innerHTML=oszlop
+        sor.appendChild(cella)
+    }
+
+    container.appendChild(sor)
+}
 
 xhttp = new XMLHttpRequest()
 
 // xhttp.onreadystatechanges = function(){
 //    if (xhttp.readyState == 4&& xhttp.status == 200){
- //       console.logC
-  //  }
+//  }
 //}
 
 xhttp.onload = function()
-    data = JSON.parse
+    data = JSON.parse(xhttp.response)
+
+
 
 xhttp.open("get",url,true)
 // CRUD - post,get,update,delete
