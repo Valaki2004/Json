a1f65884cb4ffd1d92d28d9e9a4880f4fd770dd3
vtg <varastothgergo@ktch.hu>
2024-04-03 12:24:18 +0200

    kész

--- script.js
+++ script.js
@@ -1,38 +1,67 @@
-console.log("Helló")
-url = "https://jsonplaceholder.typicode.com/posts"
+console.log("helló")
+url="http://172.16.16.148:5000/api/Humans"
 console.log(url)
 data=[]
-oszlopok = ["name","age","address","job","hobby"]
+oszlopok=["name","age","address","job","hobby"]
 
 function render(){
     container = document.getElementsByClassName("container")[0]
     container.innerHTML=""
 
+    // fejléc kialakítása
     sor = document.createElement('div')
     sor.className="row"
-
-    for(const oszlop of oszlopok){
+    for (const oszlop of oszlopok) {
         cella = document.createElement('div')
-        cella.className= "col"
+        cella.className="col"
         cella.innerHTML=oszlop
         sor.appendChild(cella)
     }
+    cella = document.createElement('div')
+    cella.className="col"
+    cella.innerHTML="Actions"
+    sor.appendChild(cella)
 
     container.appendChild(sor)
+    // fejléc vége
+    for (const human of data) {
+        sor = document.createElement('div')
+        sor.className="row"
+        sor.classList.add("my-2")
+        for (const oszlop of oszlopok) {
+            cella = document.createElement('div')
+            cella.className="col"
+
+            cella.innerHTML=human[oszlop]
+            sor.appendChild(cella)
+        }
+        cella = document.createElement('div')
+        cella.className="col"
+        gomb = document.createElement("button")
+        gomb.classList.add("btn")
+        gomb.classList.add("btn-danger")
+        gomb.innerHTML="Delete"
+        cella.appendChild(gomb)
+        sor.appendChild(cella)
+
+        container.appendChild(sor)
+    }
 }
+
 
 xhttp = new XMLHttpRequest()
 
-// xhttp.onreadystatechanges = function(){
-//    if (xhttp.readyState == 4&& xhttp.status == 200){
-//  }
-//}
+// xhttp.onreadystatechange = function(){
+//     if (xhttp.readyState==4 && xhttp.status==200){
+        
+//     }
+// }
 
-xhttp.onload = function()
-    data = JSON.parse(xhttp.response)
-
-
+xhttp.onload = function(){
+    data=JSON.parse(xhttp.responseText)
+    render();
+}
 
 xhttp.open("get",url,true)
-// CRUD - post,get,update,delete
+// CRUD - post, get, update, delete 
 xhttp.send()
