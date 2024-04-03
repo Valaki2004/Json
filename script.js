3a53cd416d4d0b5a6189350be68154f9d9724b6e
vtg <varastothgergo@ktch.hu>
2024-04-03 10:57:59 +0200

    valami

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ console.log(url)
 
 xhttp = new XMLHttpRequest()
 
+// xhttp.onreadystatechanges = function(){
+//    if (xhttp.readyState == 4&& xhttp.status == 200){
+ //       console.logC
+  //  }
+//}
+
+xhttp.onload = function()
+    data = JSON.parse
+
 xhttp.open("get",url,true)
 // CRUD - post,get,update,delete
 xhttp.send()
